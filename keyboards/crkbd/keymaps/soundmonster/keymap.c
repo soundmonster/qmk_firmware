c8ec36708a24d9c96592bab0c2bf604b2e13d666
Leo Batyuk <leonid.batyuk@wooga.net>
2019-09-24 16:40:35 +0200

    Ctrl on thumb; clean up OLED

--- keyboards/crkbd/keymaps/soundmonster/keymap.c
+++ keyboards/crkbd/keymaps/soundmonster/keymap.c
@@ -23,8 +23,10 @@ extern uint8_t is_master;
 #define _ADJUST 3
 
 // Custom keycodes for layer keys
-// Dual function escape with Lower
+// Dual function escape with left command
 #define KC_LGESC LGUI_T(KC_ESC)
+// Dual function escape with left option
+#define KC_LAESC LALT_T(KC_ESC)
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
@@ -44,11 +46,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------.                ,---------------------------------------------.
      KC_TAB,  KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,                   KC_Y,  KC_U,  KC_I,  KC_O,  KC_P,  KC_BSPC,\
   //|------+------+------+------+------+------|                |------+------+-------+------+-------+--------|
-    KC_LCTL,  KC_A,  KC_S,  KC_D,  KC_F,  KC_G,                   KC_H,  KC_J,  KC_K,  KC_L, KC_SCLN,KC_QUOT,\
+    KC_LGUI,  KC_A,  KC_S,  KC_D,  KC_F,  KC_G,                   KC_H,  KC_J,  KC_K,  KC_L, KC_SCLN,KC_QUOT,\
   //|------+------+------+------+------+------|                |------+------+-------+------+-------+--------|
     KC_LSPO,  KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,                   KC_N,  KC_M,KC_COMM,KC_DOT,KC_SLSH,KC_RSPC,\
   //|------+------+------+------+------+------+------|  |------+------+------+---0---+------+-------+--------|
-                               KC_LGESC,LOWER, KC_SPC,  KC_ENT, RAISE, KC_RALT \
+                               KC_LAESC,LOWER, KC_SPC,  KC_ENT, RAISE, KC_RCTL \
                               //`--------------------'  `--------------------'
   ),
 
@@ -56,11 +58,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,---------------------------------------------.                ,-----------------------------------------.
      KC_ESC,  KC_1, KC_2,   KC_3,   KC_4,   KC_5,                    KC_6,  KC_7,  KC_8,  KC_9,  KC_0, KC_DEL,\
   //|------+------+-------+-------+-------+-------|                |------+------+------+------+------+------|
-    KC_LCTL, KC_NO,KC_MS_L,KC_MS_D,KC_MS_U,KC_MS_R,                KC_LEFT,KC_DOWN,KC_UP,KC_RIGHT,KC_NO,KC_NO,\
+    KC_LGUI, KC_NO,KC_MS_L,KC_MS_D,KC_MS_U,KC_MS_R,                KC_LEFT,KC_DOWN,KC_UP,KC_RIGHT,KC_NO,KC_NO,\
   //|------+------+-------+-------+-------+-------|                |------+------+------+------+------+------|
     KC_LSFT, KC_NO,KC_BTN2,KC_WH_D,KC_WH_U,KC_BTN1,                KC_HOME,KC_PGDN,KC_PGUP,KC_END,KC_NO,KC_NO,\
   //|------+------+-------+-------+-------+-------+------|  |------+------+------+------+------+------+------|
-                                    KC_LGUI, LOWER,KC_SPC,   KC_ENT, RAISE,KC_RALT \
+                                    KC_LALT, LOWER,KC_SPC,   KC_ENT, RAISE,KC_RCTL \
                                   //`--------------------'  `--------------------'
   ),
 
@@ -68,11 +70,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------.                ,-----------------------------------------.
      KC_ESC,KC_EXLM,KC_AT,KC_HASH,KC_DLR,KC_PERC,              KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_BSPC,\
   //|------+------+------+------+------+------|                |------+------+------+------+------+------|
-    KC_LCTL, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                KC_MINS,KC_EQL,KC_LCBR,KC_RCBR,KC_PIPE,KC_GRV,\
+    KC_LGUI, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                KC_MINS,KC_EQL,KC_LCBR,KC_RCBR,KC_PIPE,KC_GRV,\
   //|------+------+------+------+------+------|                |------+------+------+------+------+------|
     KC_LSFT, KC_F6, KC_F7, KC_F8, KC_F9,KC_F10,                KC_UNDS,KC_PLUS,KC_LBRC,KC_RBRC,KC_BSLS,KC_TILD,\
   //|------+------+------+------+------+------+------|  |------+------+------+------+------+------+------|
-                                KC_LGUI, LOWER,KC_SPC,   KC_ENT, RAISE,KC_RALT \
+                                KC_LALT, LOWER,KC_SPC,   KC_ENT, RAISE,KC_RCTL \
                               //`--------------------'  `--------------------'
   ),
 
@@ -109,59 +111,7 @@ void matrix_init_user(void) {
     #ifdef RGBLIGHT_ENABLE
       RGB_current_mode = rgblight_config.mode;
     #endif
-    //SSD1306 OLED init, make sure to add #define SSD1306OLED in config.h
-    #ifdef SSD1306OLED
-        iota_gfx_init(!has_usb());   // turns on the display
-    #endif
 }
-
-//SSD1306 OLED update loop, make sure to add #define SSD1306OLED in config.h
-#ifdef SSD1306OLED
-
-// When add source files to SRC in rules.mk, you can use functions.
-const char *read_layer_state(void);
-const char *read_logo(void);
-void set_keylog(uint16_t keycode, keyrecord_t *record);
-const char *read_keylog(void);
-const char *read_keylogs(void);
-
-// const char *read_mode_icon(bool swap);
-// const char *read_host_led_state(void);
-// void set_timelog(void);
-// const char *read_timelog(void);
-
-void matrix_scan_user(void) {
-   iota_gfx_task();
-}
-
-void matrix_render_user(struct CharacterMatrix *matrix) {
-  if (is_master) {
-    // If you want to change the display of OLED, you need to change here
-    matrix_write_ln(matrix, read_layer_state());
-    matrix_write_ln(matrix, read_keylog());
-    //matrix_write_ln(matrix, read_keylogs());
-    //matrix_write_ln(matrix, read_mode_icon(keymap_config.swap_lalt_lgui));
-    //matrix_write_ln(matrix, read_host_led_state());
-    //matrix_write_ln(matrix, read_timelog());
-  } else {
-    matrix_write(matrix, read_logo());
-  }
-}
-
-void matrix_update(struct CharacterMatrix *dest, const struct CharacterMatrix *source) {
-  if (memcmp(dest->display, source->display, sizeof(dest->display))) {
-    memcpy(dest->display, source->display, sizeof(dest->display));
-    dest->dirty = true;
-  }
-}
-
-void iota_gfx_task_user(void) {
-  struct CharacterMatrix matrix;
-  matrix_clear(&matrix);
-  matrix_render_user(&matrix);
-  matrix_update(&display, &matrix);
-}
-#endif//SSD1306OLED
 
 #ifdef OLED_DRIVER_ENABLE
 oled_rotation_t oled_init_user(oled_rotation_t rotation) { return OLED_ROTATION_270; }
@@ -171,19 +121,19 @@ void render_space(void) {
 }
 
 void render_layer_state(void) {
-    oled_write_P(PSTR("LAYER"), false);
-    oled_write_P(PSTR("Qwrty"), layer_state_is(_QWERTY));
-    oled_write_P(PSTR("Lower"), layer_state_is(_LOWER));
-    oled_write_P(PSTR("Raise"), layer_state_is(_RAISE));
-    oled_write_P(PSTR("Adjst"), layer_state_is(_ADJUST));
+    oled_write_P(PSTR("Layer"), false);
+    oled_write_P(PSTR("RAISE"), layer_state_is(_RAISE));
+    oled_write_P(PSTR(" BASE"), layer_state_is(_QWERTY));
+    oled_write_P(PSTR("LOWER"), layer_state_is(_LOWER));
+    oled_write_P(PSTR("ADJST"), layer_state_is(_ADJUST));
 }
 
 void render_mod_status(uint8_t modifiers) {
     oled_write_P(PSTR("Mods:"), false);
-    oled_write_P(PSTR(" SHFT"), (modifiers & MOD_MASK_SHIFT));
+    oled_write_P(PSTR("SHIFT"), (modifiers & MOD_MASK_SHIFT));
     oled_write_P(PSTR(" CTRL"), (modifiers & MOD_MASK_CTRL));
-    oled_write_P(PSTR(" ALT "), (modifiers & MOD_MASK_ALT));
-    oled_write_P(PSTR(" GUI "), (modifiers & MOD_MASK_GUI));
+    oled_write_P(PSTR(" OPT "), (modifiers & MOD_MASK_ALT));
+    oled_write_P(PSTR(" CMD "), (modifiers & MOD_MASK_GUI));
 }
 
 void render_status_main(void) {
