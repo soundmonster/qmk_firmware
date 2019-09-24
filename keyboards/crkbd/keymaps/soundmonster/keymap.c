737c8513c0267233da50a76b75616eaae231cd1d
Leo Batyuk <leonid.batyuk@wooga.net>
2019-09-24 16:59:59 +0200

    Put modifiers where they belong to

--- keyboards/crkbd/keymaps/soundmonster/keymap.c
+++ keyboards/crkbd/keymaps/soundmonster/keymap.c
@@ -25,8 +25,6 @@ extern uint8_t is_master;
 // Custom keycodes for layer keys
 // Dual function escape with left command
 #define KC_LGESC LGUI_T(KC_ESC)
-// Dual function escape with left option
-#define KC_LAESC LALT_T(KC_ESC)
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
@@ -46,11 +44,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------.                ,---------------------------------------------.
      KC_TAB,  KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,                   KC_Y,  KC_U,  KC_I,  KC_O,  KC_P,  KC_BSPC,\
   //|------+------+------+------+------+------|                |------+------+-------+------+-------+--------|
-    KC_LGUI,  KC_A,  KC_S,  KC_D,  KC_F,  KC_G,                   KC_H,  KC_J,  KC_K,  KC_L, KC_SCLN,KC_QUOT,\
+    KC_LCTL,  KC_A,  KC_S,  KC_D,  KC_F,  KC_G,                   KC_H,  KC_J,  KC_K,  KC_L, KC_SCLN,KC_QUOT,\
   //|------+------+------+------+------+------|                |------+------+-------+------+-------+--------|
     KC_LSPO,  KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,                   KC_N,  KC_M,KC_COMM,KC_DOT,KC_SLSH,KC_RSPC,\
   //|------+------+------+------+------+------+------|  |------+------+------+---0---+------+-------+--------|
-                               KC_LAESC,LOWER, KC_SPC,  KC_ENT, RAISE, KC_RCTL \
+                               KC_LGESC,LOWER, KC_SPC,  KC_ENT, RAISE, KC_RALT \
                               //`--------------------'  `--------------------'
   ),
 
@@ -58,11 +56,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,---------------------------------------------.                ,-----------------------------------------.
      KC_ESC,  KC_1, KC_2,   KC_3,   KC_4,   KC_5,                    KC_6,  KC_7,  KC_8,  KC_9,  KC_0, KC_DEL,\
   //|------+------+-------+-------+-------+-------|                |------+------+------+------+------+------|
-    KC_LGUI, KC_NO,KC_MS_L,KC_MS_D,KC_MS_U,KC_MS_R,                KC_LEFT,KC_DOWN,KC_UP,KC_RIGHT,KC_NO,KC_NO,\
+    KC_LCTL, KC_NO,KC_MS_L,KC_MS_D,KC_MS_U,KC_MS_R,                KC_LEFT,KC_DOWN,KC_UP,KC_RIGHT,KC_NO,KC_NO,\
   //|------+------+-------+-------+-------+-------|                |------+------+------+------+------+------|
     KC_LSFT, KC_NO,KC_BTN2,KC_WH_D,KC_WH_U,KC_BTN1,                KC_HOME,KC_PGDN,KC_PGUP,KC_END,KC_NO,KC_NO,\
   //|------+------+-------+-------+-------+-------+------|  |------+------+------+------+------+------+------|
-                                    KC_LALT, LOWER,KC_SPC,   KC_ENT, RAISE,KC_RCTL \
+                                    KC_LGUI, LOWER,KC_SPC,   KC_ENT, RAISE,KC_RALT \
                                   //`--------------------'  `--------------------'
   ),
 
@@ -70,11 +68,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------.                ,-----------------------------------------.
      KC_ESC,KC_EXLM,KC_AT,KC_HASH,KC_DLR,KC_PERC,              KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_BSPC,\
   //|------+------+------+------+------+------|                |------+------+------+------+------+------|
-    KC_LGUI, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                KC_MINS,KC_EQL,KC_LCBR,KC_RCBR,KC_PIPE,KC_GRV,\
+    KC_LCTL, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                KC_MINS,KC_EQL,KC_LCBR,KC_RCBR,KC_PIPE,KC_GRV,\
   //|------+------+------+------+------+------|                |------+------+------+------+------+------|
     KC_LSFT, KC_F6, KC_F7, KC_F8, KC_F9,KC_F10,                KC_UNDS,KC_PLUS,KC_LBRC,KC_RBRC,KC_BSLS,KC_TILD,\
   //|------+------+------+------+------+------+------|  |------+------+------+------+------+------+------|
-                                KC_LALT, LOWER,KC_SPC,   KC_ENT, RAISE,KC_RCTL \
+                                KC_LGUI, LOWER,KC_SPC,   KC_ENT, RAISE,KC_RALT \
                               //`--------------------'  `--------------------'
   ),
 
