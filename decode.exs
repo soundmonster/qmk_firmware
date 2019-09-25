b2015f9ee2dbaf28057406716c6e7f853e8b50ca
Leo Batyuk <leonid.batyuk@wooga.net>
2019-09-26 00:36:23 +0200

    Working prototype for font decoder/encoder to PBM

--- decode.exs
+++ decode.exs
@@ -7,9 +7,13 @@ colmax = 32
 rowmax = 7
 fontw = 6
 fonth = 8
+IO.puts("P1")
+IO.puts("# This is a pbm file")
+IO.puts("#{colmax * fontw} #{rowmax * fonth}")
 
-"font.hex"
-|> File.read!()
+# "font.hex"
+# |> File.read!()
+IO.binread(:all)
 |> String.split("\n")
 |> Enum.flat_map(fn s -> String.split(s, ",") end)
 |> Enum.reject(fn s -> s == "" end)
@@ -18,15 +22,21 @@ fonth = 8
   hex
   |> String.to_integer(16)
   |> Integer.to_string(2)
-  |> String.replace("0", " ")
-  |> String.replace("1", "#")
-  |> String.pad_leading(8, " ")
+  # |> String.replace("0", " ")
+  # |> String.replace("1", "#")
+  # |> String.pad_leading(8, " ")
+  |> String.pad_leading(8, "0")
 end)
 |> Enum.chunk_every(colmax * fontw)
 |> Enum.zip()
 |> Enum.map(&Tuple.to_list/1)
 |> Enum.map(&Enum.reverse/1)
 |> Enum.map(&Enum.join/1)
+|> Enum.map(&String.to_charlist/1)
+|> Enum.map(&Enum.reverse/1)
+|> Enum.zip()
+|> Enum.map(&Tuple.to_list/1)
+|> Enum.flat_map(&Enum.chunk_every(&1, 76))
 |> Enum.each(&IO.puts/1)
 
 ## Extract glyphs
